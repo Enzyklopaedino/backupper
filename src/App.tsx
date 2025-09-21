1e667964a7b0099fa96a63fa62cd9f0b2d2d798f
Enzyklopaedino <dino2mail2@gmail.com>
2025-09-21 15:13:01 +0200

    feat: align header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Background } from './../components/background.tsx';
 const App = () => {
 	return (
 		<Background>
-			<h1 className="text-3xl font-bold text-teal-400">Backupper</h1>
+			<h1 className="text-4xl font-bold text-teal-400 h-50 w-screen p-8 flex items-center justify-center">
+				Backupper
+			</h1>
 		</Background>
 	);
 };
