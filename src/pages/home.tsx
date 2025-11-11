bd1e14f58a9bd098f4aa58e35f53306becf46a66
Enzyklopaedino <dino2mail2@gmail.com>
2025-11-11 11:42:43 +0100

    feat(home): add progress bar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,17 @@
+import { CircularProgress } from "@/components/ui/progress"
 import { FC } from "react"
 
 export const Home: FC = () => {
-  return <div>booo</div>
+  return (
+  <CircularProgress
+          value={64}
+          size={120}
+          strokeWidth={10}
+          showLabel
+          labelClassName="text-xl font-bold"
+          renderLabel={(progress) => `${progress}%`}
+          className="stroke-indigo-500/25"
+          progressClassName="stroke-indigo-600"
+        />
+  )
 }
