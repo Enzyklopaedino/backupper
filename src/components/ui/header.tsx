907c6a65c736fe1c5cae7c867d7aad75f631cce7
Enzyklopaedino <dino2mail2@gmail.com>
2025-11-21 15:23:52 +0100

    feat: add navigation to home through header click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,6 @@
 import { Squirrel } from 'lucide-react';
 import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 type HeaderProps = {
 	title: string;
@@ -7,9 +8,12 @@ type HeaderProps = {
 
 export const Header: FC<HeaderProps> = ({ title }) => {
 	return (
-		<div className="flex justify-start flex-row space-x-2 w-screen top-0 fixed py-2 px-4 border-b">
+		<Link
+			to="/"
+			className="flex justify-start flex-row space-x-2 w-screen top-0 fixed py-2 px-4 border-b"
+		>
 			<h1 className="text-2xl font-semibold">{title}</h1>
 			<Squirrel className="self-center" />
-		</div>
+		</Link>
 	);
 };
